f4d1974798b85207a052a253eeae4e8a574223e0
rithanyaa42-ops <rithanyaa42@gmail.com>
2025-12-30 12:01:36 +0530

    Update t_ff_s.v

--- t_ff_s.v
+++ t_ff_s.v
@@ -1,1 +1,18 @@
+module t_ff_sync (
+    input t, clk, rst,
+    output reg q
+);
+
+always @(posedge clk) begin
+    if (rst)
+        q = 1'b0;
+    else if (t)
+        q = ~q;
+    else
+        q = q;
+end
+
+endmodule
+
+
 
