06ff71be45b8d7264ed9a200a60cea9e7d98b03b
rithanyaa42-ops <rithanyaa42@gmail.com>
2025-12-30 12:00:21 +0530

    Add asynchronous SR flip-flop module

--- sr_ff_a.v
+++ sr_ff_a.v
@@ -1,1 +1,21 @@
+module sr_ff_async (
+    input s, r, clk, rst,
+    output reg q
+);
+
+always @(posedge clk or posedge rst) begin
+    if (rst)
+        q = 1'b0;
+    else if (s & ~r)
+        q = 1'b1;
+    else if (~s & r)
+        q = 1'b0;
+    else if (~s & ~r)
+        q = q;
+    else
+        q = 1'bx;
+end
+
+endmodule
+
 
