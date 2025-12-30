01d080c4cd4228d558c25c0bd3fd1a49a308fdd1
rithanyaa42-ops <rithanyaa42@gmail.com>
2025-12-30 11:58:42 +0530

    Update jk_ff_a.v

--- jk_ff_a.v
+++ jk_ff_a.v
@@ -1,1 +1,21 @@
+module jk_ff_async (
+    input j, k, clk, rst,
+    output reg q
+);
+
+always @(posedge clk or posedge rst) begin
+    if (rst)
+        q = 1'b0;
+    else if (j & ~k)
+        q = 1'b1;
+    else if (~j & k)
+        q = 1'b0;
+    else if (j & k)
+        q = ~q;
+    else
+        q = q;
+end
+
+endmodule
+
 
