9014404529ec293d8335644d33f20b7d45675281
rithanyaa42-ops <rithanyaa42@gmail.com>
2025-12-30 11:57:14 +0530

    Update d_ff_a.v

--- d_ff_a.v
+++ d_ff_a.v
@@ -1,1 +1,16 @@
+module d_ff_async (
+    input d, clk, rst,
+    output reg q
+);
+
+always @(posedge clk or posedge rst) begin
+    if (rst)
+        q = 1'b0;
+    else
+        q = d;
+end
+
+endmodule
+
+
 
